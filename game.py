f512e4eb14670da2576f7deac7bb0263dc0422bb
Kevin Delay <ksdelay@uvm.edu>
2018-02-13 17:50:17 -0500

    Guessing method

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import printer
+import random
 
 
 class Game:
@@ -21,7 +22,7 @@ class Game:
         self.printer = printer.Printer(self)
 
     @staticmethod
-    def compareCodes(code1, code2):
+    def compareCodes(code1, code2, self):
         # Finds the total amount of pegs cleverly
         totalPegs = sum((Counter(code1) & Counter(code2)).values())
         blackPegs = 0
@@ -29,6 +30,8 @@ class Game:
             if (code1[i] == code2[i]):
                 blackPegs += 1
         whitePegs = totalPegs - blackPegs
+        if(blackPegs==4):
+            self.didWin = True
         return blackPegs, whitePegs
     @staticmethod
     def validateGuess(codeGuess,self):
@@ -42,16 +45,59 @@ class Game:
         return (True)
     @staticmethod
     def generateSolution(self):
-        import random
-        codeString = ["", "", "", ""]
+        codeString = ["","","",""]
         for i in range(0, 4):
             codeString[i] = random.choice(self.symbolList)
         print(codeString)
         return (codeString)
+    @staticmethod
+    def makeGuess(self):
+        #Just for test
+        solution = ["R","Y","B","B"]
+        guess = ["","","",""]
+        guesses = 0
+        randVal = random.choice(self.possibleCode)
+        knownCode = []
+        ticksHad= 0
+        for i in range(0,4):
+            guess[i] = randVal
+            whiteBlacks = self.compareCodes(solution,guess,self)
+            sumTicks = whiteBlacks[0]+whiteBlacks[1]
+        while (guesses !=self.maxGuesses):
+            #prints comps last guess and increments guess
+            print(guess)
+            guesses+=1
+            #removes previous value from the list to avoid repeats
+            if(self.possibleCode):
+                self.possibleCode.remove(randVal)
+            #If the comp doesnt have all the right colors yet
+            if(sumTicks < 4 and sumTicks != 0):
+                #adds correct number of given color to known code
+                for i in range(0,sumTicks):
+                    knownCode.append(randVal)
+                    ticksHad +=1
 
-    def makeGuess():
-        raise NotImplementedError("Subclass must implement abstract method")
+            #When its zero it shuffles if not correct or closes if correct
+            elif(sumTicks ==0):
+                if(guess == solution):
+                    print("comp Won")
+                    guesses = self.maxGuesses
+                else:
+                    random.shuffle(guess)
 
+            if(self.possibleCode):
+
+                #if there are still codes in possible codes it makes a new random choice
+                #Adds random choice onto the partially correct guess
+                randVal = random.choice(self.possibleCode)
+
+                for i in range(len(knownCode),4):
+                    guess[i] = randVal
+                #Sum of ticks minus the amount already known
+                whiteBlacks = self.compareCodes(solution, guess, self)
+                sumTicks = (whiteBlacks[0] + whiteBlacks[1])-ticksHad
+
+        print("You won i think")
 
 class UserGame(Game):
     """docstring for UserGame."""
@@ -71,6 +117,9 @@ class ComputerGame(Game):
     """docstring for ComputerGame."""
     def __init__(self):
         super(ComputerGame, self).__init__()
+        self.possibleCode = ['R', 'G', 'B', 'W', 'Y', 'O']
+        self.notCodes = []
+        self.knownCodes = []
 
     # def validateGuess(codeGuess):
     #
@@ -85,8 +134,9 @@ def test():
     game = Game()
     print("Code Length: " + str(game.codeLength))
     print("Max Guesses: " + str(game.maxGuesses))
+
     print("Symbols: " + str(game.symbolList))
-    
+
 
 if (__name__ == "__main__"):
     test()
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import game
 
 
 def main():
-    game1 = game.Game()
+    game1 = game.ComputerGame()
+    test1 = ["R","R","B","Y"]
+    test2 = ["R","R","B","Y"]
+    testing = game1.compareCodes(test1,test2,game1)
+    game1.makeGuess(game1)
+    print(testing[0])
+
 
 
 if (__name__ == "__main__"):
